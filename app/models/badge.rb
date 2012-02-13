e1a61b1befc980a36df967e039ecda0820d6af68
kazuki murakami <mariamanzini8@gamil.com>
2012-02-13 23:41:56 +0900

    fix badge list

--- app/models/badge.rb
+++ app/models/badge.rb
@@ -6,7 +6,7 @@ class Badge < ActiveRecord::Base
   validates :point, numericality: {greater_than_or_equal_to: 0}
 
 
-  BADGE_IMAGES = ["badge_image/Diamond Square.gif","badge_image/Emerald Square.gif ","badge_image/Ruby Square.gif","badge_image/Sapphire Square.gif","badge_image/Princess Heart.gif"]
+  BADGE_IMAGES = ["badge_image/Diamond Square.gif","badge_image/Emerald Square.gif","badge_image/Ruby Square.gif","badge_image/Sapphire Square.gif","badge_image/Princess Heart.gif"]
 
 
 end
