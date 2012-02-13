c41edbe4000b3ea6c7084a4e43488474bf83896f
kazuki murakami <mariamanzini8@gamil.com>
2012-02-13 23:21:48 +0900

    add validation, fix forms

--- app/models/badge.rb
+++ app/models/badge.rb
@@ -1,3 +1,12 @@
 class Badge < ActiveRecord::Base
   has_many :prizes
+
+  validates :name, :point, :description, presence: true
+  validates :name, :image_url, uniqueness: true
+  validates :point, numericality: {greater_than_or_equal_to: 0}
+
+
+  BADGE_IMAGES = ["badge_image/Diamond Square.gif","badge_image/Emerald Square.gif ","badge_image/Ruby Square.gif","badge_image/Sapphire Square.gif","badge_image/Princess Heart.gif"]
+
+
 end
--- app/models/person.rb
+++ app/models/person.rb
@@ -1,3 +1,8 @@
 class Person < ActiveRecord::Base
   has_many :prizes
+
+  validates :name, :description, :image_url, presence: true
+  validates :name, uniqueness: true
+
+  PERSON_IMAGES  = ["person_image/Woof.gif", "person_image/RedDog.gif", "person_image/Bluehound.gif", "person_image/Pointy.gif", "person_image/Doggie.gif", "person_image/PurpGuy.gif"]
 end
