888a080686ca5820b68ffade3193f91667fe7814
kazuki murakami <mariamanzini8@gamil.com>
2012-03-04 23:08:42 +0900

    try fix ..

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@
 class SessionsController < ApplicationController
 
   def callback
-     raise request.env["omniauth.auth"]["info"].to_yaml
+     raise request.env["omniauth.auth"].to_yaml
 #    auth = request.env["omniauth.auth"]
 #   person = Person.find_by_provider_and_uid(auth["provider"], auth["uid"]) || Person.create_with_omniauth(auth)
 #    person = Person.where( :provider => auth["provider"], :uid => auth["uid"] ).first || Person.create_with_omniauth( auth ) 
