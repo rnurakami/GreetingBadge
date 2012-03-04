1e9a38585e44e84fdc6f616ea8897e660eaccda3
kazuki murakami <mariamanzini8@gamil.com>
2012-03-04 21:16:51 +0900

    try fix ..

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,9 @@ class SessionsController < ApplicationController
 
   def callback
     auth = request.env["omniauth.auth"]
-    person = Person.find_by_provider_and_uid(auth["provider"], auth["uid"]) || Person.create_with_omniauth(auth)
- 
+#   person = Person.find_by_provider_and_uid(auth["provider"], auth["uid"]) || Person.create_with_omniauth(auth)
+    person = Person.where( :provider => auth["provider"], :uid => auth["uid"] ).first || Person.create_with_omniauth( auth ) 
+
     session[:person_id] = person_id
     redirect_to :root, :notice => "Signed in!"
   end
