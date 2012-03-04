b8abdd4374c3ba955f9c61454b0915e95d11e802
kazuki murakami <mariamanzini8@gamil.com>
2012-03-04 22:34:23 +0900

    try fix ..

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,12 +2,13 @@
 class SessionsController < ApplicationController
 
   def callback
-    auth = request.env["omniauth.auth"]
+     raize request.env["omniauth.auth"].to_yaml
+#    auth = request.env["omniauth.auth"]
 #   person = Person.find_by_provider_and_uid(auth["provider"], auth["uid"]) || Person.create_with_omniauth(auth)
-    person = Person.where( :provider => auth["provider"], :uid => auth["uid"] ).first || Person.create_with_omniauth( auth ) 
+#    person = Person.where( :provider => auth["provider"], :uid => auth["uid"] ).first || Person.create_with_omniauth( auth ) 
 
-    session[:person_id] = person.id
-    redirect_to :timeline, :notice => "Signed in!"
+#    session[:person_id] = person.id
+#    redirect_to :timeline, :notice => "Signed in!"
   end
 
   def destroy
