6ca47b347601ee30fa0a6b2f5c903fb310b1a530
kazuki murakami <mariamanzini8@gamil.com>
2012-03-24 14:37:52 +0900

    change timeline order

--- app/models/prize.rb
+++ app/models/prize.rb
@@ -2,4 +2,8 @@ class Prize < ActiveRecord::Base
   belongs_to :award_person, :class_name=>'Person', :foreign_key=>'award_person_id'
   belongs_to :receive_person, :class_name=>'Person', :foreign_key=>'receive_person_id'
   belongs_to :badge
+
+  validates_presence_of :award_person
+  validates_presence_of :receive_person
+  validates_presence_of :badge
 end
